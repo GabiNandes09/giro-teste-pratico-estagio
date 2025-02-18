6e400e583cdf4a40c987a8029e4065726782534e
Gabriel Fernandes <fernandes.gabrielgon@gmail.com>
2025-02-18 10:09:44 -0300

    Metodos GetALL Validados

--- giro_tech/src/main/java/com/gabrielfernandes/giro_tech/entity/CurrencyEntity.java
+++ giro_tech/src/main/java/com/gabrielfernandes/giro_tech/entity/CurrencyEntity.java
@@ -1,11 +1,6 @@
 package com.gabrielfernandes.giro_tech.entity;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.*;
 
--- giro_tech/src/main/java/com/gabrielfernandes/giro_tech/service/ExchangeRateService.java
+++ giro_tech/src/main/java/com/gabrielfernandes/giro_tech/service/ExchangeRateService.java
@@ -1,8 +1,6 @@
 package com.gabrielfernandes.giro_tech.service;
 
-import java.time.Instant;
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
